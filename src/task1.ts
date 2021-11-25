5bdcb90a01afb250f2aa4b000763a8528998442a
43p3nawka <43p3nawka@protonmail.com>
2021-11-26 01:59:07 +0300

    Init and add task 1 for reversing text from prompt

---
+++ src/task1.ts
@@ -0,0 +1,18 @@
+import { stdin, stdout } from 'process';
+import { Transform, Writable } from 'stream';
+
+const transformStream = new Transform();
+
+transformStream._transform = (chunk, enconding, callback) => {
+	transformStream.push(chunk.toString().split('').reverse().join(''));
+	callback();
+};
+
+const writableStream = new Writable();
+
+writableStream._write = (chunk, enconding, callback) => {
+	stdout.write(`${chunk.toString()}\n\n`);
+	callback();
+};
+
+stdin.pipe(transformStream).pipe(writableStream);
